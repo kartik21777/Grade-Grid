877dfbc754b936596f61493a33eb190f3efc9bb1
Achintya Bist <achintyabist05@gmail.com>
2026-03-17 23:13:46 +0530

    added card css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Card = ({styles, heading, text, linkTo}) => {
   return (
       <Link to={linkTo} style={{ textDecoration: "none", color: "inherit" }}>
-        <div style={styles}>
+        <div className={styles}>
           <h3>{heading}</h3>
           <p>{text}</p>
         </div>
--- src/pages/teacher/TeacherHome.jsx
+++ src/pages/teacher/TeacherHome.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Card from '../../components/Card';
 
-const TeacherHome = ({ styles }) => (
-  <div style={{ display: 'flex', gap: '20px', flexWrap: 'wrap' }}>
-    <Card styles={{ ...styles.card, borderTop: '4px solid #ea4335' }} heading={'📅 Schedule'} text={'View your weekly timetable'} linkTo={'schedule'}/>
+const TeacherHome = () => (
+  <div className='card-container'>
+    <Card styles="card card-schedule" heading={'📅 Schedule'} text={'View your weekly timetable'} linkTo={'schedule'}/>
 
-    <Card styles={{ ...styles.card, borderTop: '4px solid #34a853' }} heading={'📚 Classes'} text={'Manage sections & assignments'} linkTo={'classes'}/>
+    <Card styles="card card-class" heading={'📚 Classes'} text={'Manage sections & assignments'} linkTo={'classes'}/>
 
-    <Card styles={{ ...styles.card, borderTop: '4px solid #3538ea' }} heading={'⏰ Assignments'} text={'Track upcoming due dates'} linkTo={'assignments'}/>
+    <Card styles="card card-assignments" heading={'⏰ Assignments'} text={'Track upcoming due dates'} linkTo={'assignments'}/>
   </div>
 );
 
